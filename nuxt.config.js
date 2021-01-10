f3a04415571075c758444b9f88bcd3911eb772f4
Ebrahim Maher <hema.developer.74@gmail.com>
2021-01-10 21:22:32 +0200

    Comparison Expression Lesson

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'theme-color', content: '#4927d0' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' },
       { hid: 'og:site_name', property: 'og:site_name', content: 'دليل مبرمج' },
       { hid: 'og:type', property: 'og:type', content: 'website' },
       {
