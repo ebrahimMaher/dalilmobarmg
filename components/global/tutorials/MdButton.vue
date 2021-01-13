77d386d9d4855987a1bb88f7b740a9b26b8e3bdf
Ebrahim Maher <hema.developer.74@gmail.com>
2021-01-13 21:31:59 +0200

    Environment Setup lesson

---
+++ components/global/tutorials/MdButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <a target="_blank" :class="{'w-full': full}" class="block text-center btn cursor-pointer px-4 py-2 font-bold text-sm shadow-md bg-primary-base text-white hover:bg-primary-dark transform active:scale-95 transition duration-150 rounded">
+    <slot />
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    full: {type: Boolean, default: true}
+  },
+
+}
+</script>
+
+<style>
+
+</style>
